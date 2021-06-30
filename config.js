d9150180bb4babcdc318331a573fe6ddaf6aa830
pqvittt <86726218+pqvittt@users.noreply.github.com>
2021-06-30 20:23:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    introTitle: 'Cậu đây rồi, tớ nói nè!',
+    introDesc: Hôm nay anh học Toán hình
+    Tròn vuông chẳng có
+    Toàn hình bóng em
+    Anh nói thật,
+    btnIntro: 'Anh nói nè',
+    title: 'Anh có thể không có gì nhưng không thể không có em 😙',
+    desc: 'Anh có thể mất tất cả nhưng không thể mất em.  ',
+    btnYes: 'Vâng, em thích anh <33',
+    btnNo: 'Không, mơ à, ai thèm :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Em thích anh lắm đó, hihihihi <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Thế giờ anh quá đón công chúa của anh nha,sao rời được 1s.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com/PhanQuocVietOfficial' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
